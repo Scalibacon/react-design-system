d071f42c3ec5bb3767de19df7c0fa0b9f9161b72
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2022-10-13 22:38:50 -0300

    add github workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,13 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  // caso tenha "/" no nome do usuário/repositório
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION'){
+      config.base = '/react-design-system/';
+    }
+
+    return config;
   }
 }
